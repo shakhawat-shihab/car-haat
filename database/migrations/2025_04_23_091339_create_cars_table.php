c0ef880bc6523f8b8a86daf637ca85d9a478134b
Shakhawat-Github <shihabchtg@gmail.com>
2025-04-24 09:00:37 +0600

    feat: creating  migration files for all the tasbles

---
+++ database/migrations/2025_04_23_091339_create_cars_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->integer("year");
+            $table->string("price");
+            $table->string("vin");
+            $table->string("mileage");
+            $table->string("address");
+            $table->string("phone",45);
+            $table->longText("description")->nullable();
+            $table->timestamp("published_at")->nullable();
+            $table->timestamp("deleted_at")->nullable();
+            $table->foreignId('marker_id')->constrained('makes');
+            $table->foreignId('model_id')->constrained('models');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId("car_type_id")->constrained("car_types");
+            $table->foreignId("fuel_type_id")->constrained("fuel_types");
+            $table->foreignId("city_id")->constrained("cities");
+            $table->string("color");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
